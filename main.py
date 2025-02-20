687c346a04024d7bdffef17bd384c74a96343c5e
Vinícius Santos <vinisantoosk@gmail.com>
2025-02-20 18:00:21 -0300

    feat: configs

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request, jsonify
 import torch
 import cv2
@@ -56,4 +58,5 @@ def capture():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
